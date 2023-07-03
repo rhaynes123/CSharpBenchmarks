1c99f548801a769bc8974cf65aaa1b80b594afc1
Richard <rahayn@yahoo.com>
2023-07-03 19:30:40 -0400

    adding loop examples

---
+++ CSharpBenchMarks/CSharpBenchMarksForLoops/ListForLoopTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchMarksForLoops
+{
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.SlowestToFastest)]
+    [RankColumn]
+    public class ListForLoopTests
+    {
+        private List<int> TestSample = Enumerable.Range(0, 1000).ToList();
+        [Benchmark]
+        public void ForEachLoop()
+        {
+            int count = 0;
+            foreach (var item in TestSample)
+            {
+                count++;
+            }
+        }
+        [Benchmark]
+        public void ForEachLoopMethod()
+        {
+            int count = 0;
+            TestSample.ForEach(sample => count++);
+        }
+        [Benchmark]
+        public void ForEachSelectMethod()
+        {
+            int count = 0;
+            TestSample.Select(sample => count++);
+        }
+    }
+}
+
---
+++ CSharpBenchMarks/CSharpBenchMarksForLoops/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using CSharpBenchMarksForLoops;
+
+BenchmarkRunner.Run<ListForLoopTests>();
+
